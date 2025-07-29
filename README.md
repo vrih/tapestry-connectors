a972e9f9271ed8c57a0fe8e1581a12d08d212153
Daniel Bowman <daniel@danielbowman.co.uk>
2025-07-29 16:04:44 +0100

    Add Sonarr tapestry connector

---
+++ README.md
@@ -0,0 +1,3 @@
+# Connectors for tapestry
+
+* Sonarr - adds imported to episodes to feed with a look back of 1 day
---
+++ com.danielbowman.sonarr/plugin.js
@@ -0,0 +1,58 @@
+function getData(path) {
+        const headers = {"X-Api-Key": apiKey, "accept": "application/json"};
+        return sendRequest(path, "GET", null, headers);
+}
+
+function load(){
+        let date = new Date();
+        date.setDate(date.getDate() - 1);
+        const endpoint = site + "/api/v3/history/since?eventType=downloadFolderImported&date=" + date.toISOString();
+        getData(endpoint)
+                .then((text) => JSON.parse(text))
+                .then((json) => {
+                        const items = json.map((el) => {
+                                console.log(el);
+                                return getData(site + "/api/v3/episode/" + el.episodeId)
+                                        .then((response) => { 
+                                                console.log(response);
+                                                const d = JSON.parse(response);
+                                                let elDate = new Date(el.date);
+                                                let elUri = endpoint + `&id=${el.id}`;
+                                                let item = Item.createWithUriDate(elUri, elDate)
+                                                item.title = `${d.series.title} - ${d.title} S${d.seasonNumber}E${d.episodeNumber}`
+                                                item.body = `<p>${d.overview}</p>`
+
+                                                if(d.images[0].remoteUrl != undefined) {
+                                                        let mediaAttachment = MediaAttachment.createWithUrl(d.images[0].remoteUrl);
+                                                        item.attachments = [mediaAttachment];
+                                                }
+                                                return item;
+                                        })
+                        });
+                        console.log(items);
+                        return Promise.all(items);
+                })
+                .then((items) => {
+                        console.log(items);
+                        processResults(items);
+                })
+                .catch((requestError) => {
+                        processError(requestError);
+                });
+
+
+
+}
+
+function verify(){
+  verifyAsync().then(processVerification).catch(processError)
+
+}
+
+async function verifyAsync() {
+  const url = `${site}/api/v3/ping`
+  const check = JSON.parse(await getData(url))
+  // all we need to know is that getting data didn't fail
+  return "Healthchecks"
+}
+
