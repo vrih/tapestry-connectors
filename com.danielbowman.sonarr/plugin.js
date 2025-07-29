2ed13d03a0de62c5794721998b38f0d77e547c50
Daniel Bowman <daniel@danielbowman.co.uk>
2025-07-29 16:07:05 +0100

    Clean up formatting

--- com.danielbowman.sonarr/plugin.js
+++ com.danielbowman.sonarr/plugin.js
@@ -1,44 +1,40 @@
 function getData(path) {
-        const headers = {"X-Api-Key": apiKey, "accept": "application/json"};
-        return sendRequest(path, "GET", null, headers);
+  const headers = {"X-Api-Key": apiKey, "accept": "application/json"};
+  return sendRequest(path, "GET", null, headers);
 }
 
 function load(){
-        let date = new Date();
-        date.setDate(date.getDate() - 1);
-        const endpoint = site + "/api/v3/history/since?eventType=downloadFolderImported&date=" + date.toISOString();
-        getData(endpoint)
-                .then((text) => JSON.parse(text))
-                .then((json) => {
-                        const items = json.map((el) => {
-                                console.log(el);
-                                return getData(site + "/api/v3/episode/" + el.episodeId)
-                                        .then((response) => { 
-                                                console.log(response);
-                                                const d = JSON.parse(response);
-                                                let elDate = new Date(el.date);
-                                                let elUri = endpoint + `&id=${el.id}`;
-                                                let item = Item.createWithUriDate(elUri, elDate)
-                                                item.title = `${d.series.title} - ${d.title} S${d.seasonNumber}E${d.episodeNumber}`
-                                                item.body = `<p>${d.overview}</p>`
+  let date = new Date();
+  date.setDate(date.getDate() - 1);
+  const endpoint = site + "/api/v3/history/since?eventType=downloadFolderImported&date=" + date.toISOString();
+  getData(endpoint)
+    .then((text) => JSON.parse(text))
+    .then((json) => {
+      const items = json.map((el) => {
+        return getData(site + "/api/v3/episode/" + el.episodeId)
+          .then((response) => { 
+            const d = JSON.parse(response);
+            let elDate = new Date(el.date);
+            let elUri = endpoint + `&id=${el.id}`;
+            let item = Item.createWithUriDate(elUri, elDate)
+            item.title = `${d.series.title} - ${d.title} S${d.seasonNumber}E${d.episodeNumber}`
+            item.body = `<p>${d.overview}</p>`
 
-                                                if(d.images[0].remoteUrl != undefined) {
-                                                        let mediaAttachment = MediaAttachment.createWithUrl(d.images[0].remoteUrl);
-                                                        item.attachments = [mediaAttachment];
-                                                }
-                                                return item;
-                                        })
-                        });
-                        console.log(items);
-                        return Promise.all(items);
-                })
-                .then((items) => {
-                        console.log(items);
-                        processResults(items);
-                })
-                .catch((requestError) => {
-                        processError(requestError);
-                });
+            if(d.images[0].remoteUrl != undefined) {
+              let mediaAttachment = MediaAttachment.createWithUrl(d.images[0].remoteUrl);
+              item.attachments = [mediaAttachment];
+            }
+            return item;
+          })
+      });
+      return Promise.all(items);
+    })
+    .then((items) => {
+      processResults(items);
+    })
+    .catch((requestError) => {
+      processError(requestError);
+    });
 
 
 
